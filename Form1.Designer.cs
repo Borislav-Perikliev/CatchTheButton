c39074af57e2134845f260aff5100d7df138f8ed
ボビー Perikliev <aryan15051990@outlook.com>
2023-12-09 21:09:35 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CatchTheButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCatchMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCatchMe
+            // 
+            this.buttonCatchMe.BackColor = System.Drawing.Color.PeachPuff;
+            this.buttonCatchMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCatchMe.Location = new System.Drawing.Point(12, 12);
+            this.buttonCatchMe.Name = "buttonCatchMe";
+            this.buttonCatchMe.Size = new System.Drawing.Size(55, 46);
+            this.buttonCatchMe.TabIndex = 0;
+            this.buttonCatchMe.Text = "Catch me!";
+            this.buttonCatchMe.UseVisualStyleBackColor = false;
+            this.buttonCatchMe.Click += new System.EventHandler(this.buttonCatchMe_Click);
+            this.buttonCatchMe.MouseEnter += new System.EventHandler(this.buttonCatchMe_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(424, 341);
+            this.Controls.Add(this.buttonCatchMe);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CATCH THE BUTTON!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCatchMe;
+    }
+}
+
